0431b091b3851bcf88886ac8dab6cf4cd9fe81dd
Daesing <peterpauler77@naver.com>
2024-10-05 01:39:31 +0900

    실습3 완성

--- OpenGL_basic/practice3.cpp
+++ OpenGL_basic/practice3.cpp
@@ -3,17 +3,46 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <GL/freeglut_ext.h>
+#define WIDTH 800
+#define HEIGHT 600
+
+CONST INT RECTCNT = 10;
 
 std::random_device rd;
 std::default_random_engine dre(rd());
 std::uniform_real_distribution<float> urd{ 0.0f,1.0f };
+std::uniform_int_distribution uid{ 100,600 };
+
+struct Rect {
+    int x{ uid(dre) }, y{ uid(dre) };
+    GLfloat r{ urd(dre) }, g{ urd(dre) }, b{ urd(dre) };
+    int size{ 20 };
+    bool isActive{ false };
+
+};
+
+Rect r[10];
+int currentIdx{};
+bool left_button{ false };
+int currentRect{};
+
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 void TimerFunction(int value);
-
 bool timerActive{ false };
+void Mouse(int button, int state, int x, int y);
+void Motion(int x, int y);
+void checkCrash();
+
+float convertX(int x) {
+    return ((float)x / (WIDTH / 2)) - 1.f;
+}
+float convertY(int y) {
+    return 1.f - ((float)y / (HEIGHT / 2));
+}
+
 
 int main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정
     //--- 윈도우 생성하기
@@ -36,14 +65,24 @@ int main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정
     glutDisplayFunc(drawScene); //--- 출력 콜백함수의 지정
     glutReshapeFunc(Reshape); //--- 다시 그리기 콜백함수 지정
     glutKeyboardFunc(Keyboard); //--- 키보드 입력 콜백함수 지정
-    glutTimerFunc(1000, TimerFunction, 1);
+    glutMouseFunc(Mouse);
+    glutMotionFunc(Motion);
     glutMainLoop(); //--- 이벤트 처리 시작
 }
 
 GLvoid drawScene() { //--- 콜백 함수: 그리기 콜백 함수
     //--- 변경된 배경색 설정
-    //glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //--- 바탕색을 변경 (예: 검은색)
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //--- 바탕색을 변경 (예: 검은색)
     glClear(GL_COLOR_BUFFER_BIT); //--- 설정된 색으로 전체를 칠하기
+
+    for (int i = 0; i < RECTCNT; ++i) {
+        if (r[i].isActive) {
+            glColor3f(r[i].r, r[i].g, r[i].b);
+            glRectf(convertX(r[i].x - r[i].size), convertY(r[i].y - r[i].size), convertX(r[i].x + r[i].size), convertY(r[i].y + r[i].size));
+        }
+    }
+
+
     glutSwapBuffers(); //--- 화면에 출력하기
 }
 
@@ -56,7 +95,11 @@ GLvoid Keyboard(unsigned char key, int x, int y) { //--- 키보드 입력 처리
     switch (key)
     {
     case 'a':
-
+        if (currentIdx < 10) {
+            r[currentIdx].isActive = true;
+            ++currentIdx;
+        }
+        break;
     default:
         break;
     }
@@ -64,16 +107,68 @@ GLvoid Keyboard(unsigned char key, int x, int y) { //--- 키보드 입력 처리
     glutPostRedisplay(); //--- 배경색이 바뀔 때마다 출력 콜백 함수를 호출하여 화면을 refresh 한다
 }
 
-void TimerFunction(int value)
+
+void Mouse(int button, int state, int x, int y)
 {
-    GLclampf red{ urd(dre) }, green{ urd(dre) }, blue{ urd(dre) };
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        left_button = true;
 
-    if (!timerActive)
-        return;
+        for (int i = 0; i < RECTCNT; ++i) {
+            if (x < r[i].x + r[i].size && x > r[i].x - r[i].size && y < r[i].y + r[i].size && y > r[i].y - r[i].size) {
+                currentRect = i;
+                r[i].x = x;
+                r[i].y = y;
+            }
+        }
 
-    glClearColor(red, green, blue, 1.0f);
-    glutPostRedisplay(); // 화면 재 출력
-    glutTimerFunc(1000, TimerFunction, 1); // 타이머함수 재 설정
+    }
+    else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+        left_button = false;
+        currentRect = -1;
+    }
+    glutPostRedisplay();
+}
+
+void Motion(int x, int y)
+{
+    
+
+    if (currentRect != -1) {
+        r[currentRect].x = x;
+        r[currentRect].y = y;
+    }
+
+    checkCrash();
+
+    glutPostRedisplay();
+}
+
+void checkCrash() {
+    int temp_x{}, temp_y{};
+
+    for (int i = 0; i < RECTCNT; ++i) {
+        for (int j = currentRect; j < currentRect+1; ++j) {
+            if (i == j)
+                continue;
+            if (r[i].x - r[i].size < r[j].x + r[j].size && r[i].y - r[i].size < r[j].y + r[j].size
+                && r[i].x + r[i].size>r[j].x - r[j].size && r[i].y + r[i].size>r[j].y - r[j].size && r[i].isActive && r[j].isActive) {
+
+                temp_x = ((r[i].x * r[i].size) + (r[j].x * r[j].size)) / (r[i].size + r[j].size);
+                temp_y = ((r[i].y * r[i].size) + (r[j].y * r[j].size)) / (r[i].size + r[j].size);
+                r[j].size += r[i].size;
+                r[i].isActive = false;
+
+                r[j].x = temp_x;
+                r[j].y = temp_y;
+                r[j].r = urd(dre);
+                r[j].g = urd(dre);
+                r[j].b = urd(dre);
+
+
+            }
+        }
+    }
+    glutPostRedisplay();
 }
 
 
