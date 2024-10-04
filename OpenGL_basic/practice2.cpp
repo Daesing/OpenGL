0f58856aca648a178e7251f29d9604f3bd5df77a
Daesing <peterpauler77@naver.com>
2024-10-04 19:43:49 +0900

    실습2 완성

--- OpenGL_basic/practice2.cpp
+++ OpenGL_basic/practice2.cpp
@@ -1,51 +1,63 @@
-#include <iostream>
-#include <GL/glew.h> // 필요한 헤더파일 include
+#include <iostream> //--- 필요한 헤더파일 include
+#include <random>
+#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <GL/freeglut_ext.h>
-#include <random>
-
-CONST INT WIDTH = 800;
-CONST INT HEIGHT = 600;
 
 std::random_device rd;
 std::default_random_engine dre(rd());
 std::uniform_real_distribution<float> urd{ 0.0f,1.0f };
 
-void drawScene(); // 그리기 함수 선언
-void Reshape(int w, int h); // 크기 조정 함수 선언
-void Mouse(int button, int state, int x, int y);
-float modify_xcord(int x);
-float modify_ycord(int y);
-
 struct Rect {
-    float xStart, yStart, xEnd, yEnd;
-    float r{ urd(dre) }, g{ urd(dre) }, b{ urd(dre) };
-    
-    Rect(int xStart_, int yStart_, int xEnd_, int yEnd_) {
-        xStart = xStart_;
-        yStart = yStart_;
-        xEnd = xEnd_;
-        yEnd = yEnd_;
-    }
+    int x1, x2, y1, y2;
+    float r{ urd(dre) }, g{ urd(dre) }, b{ urd(dre)};
+    int size_cnt{ 10 };
 
+    Rect(int x1_, int y1_, int x2_, int y2_)
+        : x1(x1_), y1(y1_), x2(x2_), y2(y2_) {
+    }
 };
 
-Rect rect1(-1, 1, 0, 0);
-Rect rect2(0, 1, 1, 0);
-Rect rect3(-1, 0, 0, -1);
-Rect rect4(0, 0, 1, -1);
+Rect r[4] = {
+        Rect(0, 0, 400, 300),
+        Rect(400, 0, 800, 300),
+        Rect(0, 300, 400, 600),
+        Rect(400, 300, 800, 600)
+};
 
-int main(int argc, char** argv) { // 윈도우 출력하고 콜백함수 설정
-    // 윈도우 생성하기
-    glutInit(&argc, argv); // GLUT 초기화
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정
-    glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
-    glutInitWindowSize(800, 600); // 윈도우의 크기 지정
-    glutCreateWindow("Example1"); // 윈도우 생성 (윈도우 이름)
 
-    // GLEW 초기화하기
+
+CONST INT WIDTH = 800;
+CONST INT HEIGHT = 600;
+
+float convertX(int x) {
+    return ((float)x / (WIDTH / 2)) - 1.f;
+}
+float convertY(int y) {
+    return 1.f - ((float)y / (HEIGHT / 2));
+}
+
+
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+void TimerFunction(int value);
+GLvoid Mouse(int button, int state, int x, int y);
+bool timerActive{ false };
+
+
+int main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정
+    //--- 윈도우 생성하기
+    glutInit(&argc, argv); //--- glut 초기화
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //--- 디스플레이 모드 설정
+    glutInitWindowPosition(0, 0); //--- 윈도우의 위치 지정
+    glutInitWindowSize(800, 600); //--- 윈도우의 크기 지정
+    glutCreateWindow("Example1"); //--- 윈도우 생성 (윈도우 이름)
+
+    //--- GLEW 초기화하기
     glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) { // GLEW 초기화
+    if (glewInit() != GLEW_OK) { //--- glew 초기화
         std::cerr << "Unable to initialize GLEW" << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -53,102 +65,137 @@ int main(int argc, char** argv) { // 윈도우 출력하고 콜백함수 설정
         std::cout << "GLEW Initialized\n";
     }
 
-   
-    glutDisplayFunc(drawScene); // 출력 함수 지정
+    glutDisplayFunc(drawScene); //--- 출력 콜백함수의 지정
+    glutReshapeFunc(Reshape); //--- 다시 그리기 콜백함수 지정
+    glutKeyboardFunc(Keyboard); //--- 키보드 입력 콜백함수 지정
     glutMouseFunc(Mouse);
-    glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
-    glutMainLoop(); // 이벤트 처리 시작
-
-    return 0; // main 함수 종료
+    glutMainLoop(); //--- 이벤트 처리 시작
 }
 
-void drawScene() { // 그리기 콜백 함수
-    glColor3f(rect1.r, rect1.g, rect1.b);
-    glRectf(rect1.xStart, rect1.yStart, rect1.xEnd, rect1.yEnd);
+GLvoid drawScene() { //--- 콜백 함수: 그리기 콜백 함수
+    //--- 변경된 배경색 설정
+    
+    glClear(GL_COLOR_BUFFER_BIT); //--- 설정된 색으로 전체를 칠하기
 
-    glColor3f(rect2.r, rect2.g, rect2.b);
-    //glRectf(rect2.xStart, rect2.yStart, rect2.xEnd, rect2.yEnd);
+    for (int i = 0; i < 4; ++i) {
+        glColor3f(r[i].r, r[i].g, r[i].b);
+        glRectf(convertX(r[i].x1), convertY(r[i].y1), convertX(r[i].x2), convertY(r[i].y2));
+    }
 
-    glColor3f(rect3.r, rect3.g, rect3.b);
-    glRectf(rect3.xStart, rect3.yStart, rect3.xEnd, rect3.yEnd);
 
-    glColor3f(rect4.r, rect4.g, rect4.b);
-    glRectf(rect4.xStart, rect4.yStart, rect4.xEnd, rect4.yEnd);
 
-    glutSwapBuffers(); // 화면에 출력하기
+
+    glutSwapBuffers(); //--- 화면에 출력하기
 }
 
-void Reshape(int w, int h) { // 크기 조정 콜백 함수
+GLvoid Reshape(int w, int h) { //--- 콜백 함수: 다시 그리기 콜백 함수
     glViewport(0, 0, w, h);
 }
 
+GLvoid Keyboard(unsigned char key, int x, int y) { //--- 키보드 입력 처리
 
-void Mouse(int button, int state, int x, int y)
-{
+    glutPostRedisplay(); //--- 배경색이 바뀔 때마다 출력 콜백 함수를 호출하여 화면을 refresh 한다
+}
 
-    float modify_x{ modify_xcord(x) }, modify_y{ modify_ycord(y) };
+GLvoid Mouse(int button, int state, int x, int y) {
 
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        if ((modify_x < rect1.xEnd) && (rect1.xStart < modify_x) && (modify_y < rect1.yStart) && (rect1.yEnd < modify_y)) {
-            rect1.r = urd(dre); rect1.g = urd(dre); rect1.b = urd(dre);
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+
+		if (x > r[0].x1 && x < r[0].x2 && y > r[0].y1 && y < r[0].y2) {
+			r[0].r = urd(dre);
+			r[0].g = urd(dre);
+			r[0].b = urd(dre);
+		}
+        else if (x > r[1].x1 && x < r[1].x2 && y > r[1].y1 && y < r[1].y2) {
+            r[1].r = urd(dre);
+            r[1].g = urd(dre);
+            r[1].b = urd(dre);
         }
-        else if ((modify_x < rect2.xEnd) && (rect2.xStart < modify_x) && (modify_y > rect2.yEnd) && (rect2.yStart > modify_y)) {
-            rect2.r = urd(dre); rect2.g = urd(dre); rect2.b = urd(dre);
+        else if (x > r[2].x1 && x < r[2].x2 && y > r[2].y1 && y < r[2].y2) {
+            r[2].r = urd(dre);
+            r[2].g = urd(dre);
+            r[2].b = urd(dre);
         }
-        else if ((modify_x < rect3.xEnd) && (rect3.xStart < modify_x) && (modify_y > rect3.yEnd) && (rect3.yStart > modify_y)) {
-            rect3.r = urd(dre); rect3.g = urd(dre); rect3.b = urd(dre);
-        }
-        else if ((modify_x < rect4.xEnd) && (rect4.xStart < modify_x) && (modify_y > rect4.yEnd) && (rect4.yStart > modify_y)) {
-            rect4.r = urd(dre); rect4.g = urd(dre); rect4.b = urd(dre);
+        else if (x > r[3].x1 && x < r[3].x2 && y > r[3].y1 && y < r[3].y2) {
+            r[3].r = urd(dre);
+            r[3].g = urd(dre);
+            r[3].b = urd(dre);
         }
         else {
-            
-        }
-    }
-    else if (GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-        if ((modify_x < rect1.xEnd) && (rect1.xStart < modify_x) && (modify_y < rect1.yStart) && (rect1.yEnd < modify_y)) {
-            rect1.xStart += 0.1;
-            rect1.xEnd -= 0.1;
-            rect1.yStart -= 0.1;
-            rect1.yEnd += 0.1;
-        }
-        else if ((modify_x < rect2.xEnd) && (rect2.xStart < modify_x) && (modify_y > rect2.yEnd) && (rect2.yStart > modify_y)) {
-            rect2.xStart += 0.1;
-            rect2.xEnd -= 0.1;
-            rect2.yStart -= 0.1;
-            rect2.yEnd += 0.1;
-        }
-        else if ((modify_x < rect3.xEnd) && (rect3.xStart < modify_x) && (modify_y > rect3.yEnd) && (rect3.yStart > modify_y)) {
-            rect3.xStart += 0.1;
-            rect3.xEnd -= 0.1;
-            rect3.yStart -= 0.1;
-            rect3.yEnd += 0.1;
-        }
-        else if ((modify_x < rect4.xEnd) && (rect4.xStart < modify_x) && (modify_y > rect4.yEnd) && (rect4.yStart > modify_y)) {
-            rect4.xStart += 0.1;
-            rect4.xEnd -= 0.1;
-            rect4.yStart -= 0.1;
-            rect4.yEnd += 0.1;
-        }
-        else {
-            
+            glClearColor(urd(dre), urd(dre), urd(dre), 1.0f);
         }
 
     }
+    if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+
+        if (x > r[0].x1 && x < r[0].x2 && y > r[0].y1 && y < r[0].y2) {
+            if (r[0].size_cnt != 0) {
+                r[0].x1 += 10;
+                r[0].y1 += 10;
+                r[0].x2 -= 10;
+                r[0].y2 -= 10;
+                r[0].size_cnt -= 1;
+            }
+            
+        }
+        else if (x > r[1].x1 && x < r[1].x2 && y > r[1].y1 && y < r[1].y2) {
+            if (r[1].size_cnt != 0) {
+                r[1].x1 += 10;
+                r[1].y1 += 10;
+                r[1].x2 -= 10;
+                r[1].y2 -= 10;
+                r[1].size_cnt -= 1;
+            }
+        }
+        else if (x > r[2].x1 && x < r[2].x2 && y > r[2].y1 && y < r[2].y2) {
+            if (r[2].size_cnt != 0) {
+                r[2].x1 += 10;
+                r[2].y1 += 10;
+                r[2].x2 -= 10;
+                r[2].y2 -= 10;
+                r[2].size_cnt -= 1;
+            }
+        }
+        else if (x > r[3].x1 && x < r[3].x2 && y > r[3].y1 && y < r[3].y2) {
+            if (r[3].size_cnt != 0) {
+                r[3].x1 += 10;
+                r[3].y1 += 10;
+                r[3].x2 -= 10;
+                r[3].y2 -= 10;
+                r[3].size_cnt -= 1;
+            }
+        }
+        //오른쪽 클릭 사각형 외부
+        else if (x > 0 && x < 400 && y>0 && y < 300) {
+            r[0].x1 -= 10;
+            r[0].y1 -= 10;
+            r[0].x2 += 10;
+            r[0].y2 += 10;
+            r[0].size_cnt += 1;
+        }
+        else if (x > 400 && x < 800 && y>0 && y < 300) {
+            r[1].x1 -= 10;
+            r[1].y1 -= 10;
+            r[1].x2 += 10;
+            r[1].y2 += 10;
+            r[1].size_cnt += 1;
+        }
+        else if (x > 0 && x < 400 && y>300 && y < 600) {
+            r[2].x1 -= 10;
+            r[2].y1 -= 10;
+            r[2].x2 += 10;
+            r[2].y2 += 10;
+            r[2].size_cnt += 1;
+        }
+        else if (x > 0 && x < 800 && y>300 && y < 600) {
+            r[3].x1 -= 10;
+            r[3].y1 -= 10;
+            r[3].x2 += 10;
+            r[3].y2 += 10;
+            r[3].size_cnt += 1;
+        }
+    }
+
+
 }
 
-float modify_xcord(int x) {
-    float modify_x;
-    modify_x = static_cast<float>(x);
-    modify_x = (x - WIDTH / 2.0) / (WIDTH / 2.0);
-    
-    return modify_x;
-
-}
-float modify_ycord(int y) {
-    float modify_y;
-    modify_y = static_cast<float>(y);
-    modify_y = (HEIGHT / 2.0 - y) / (HEIGHT / 2.0);
-
-    return modify_y;
-}
