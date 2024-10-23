6af17aafef67e8ff5fe52f6afda1cd6f574f823a
Daesing <149518869+Daesing@users.noreply.github.com>
2024-10-23 17:17:37 +0900

    실습 13번 투영변환, 정육면체 그림

--- Shader_3D_Basic/practice13.cpp
+++ Shader_3D_Basic/practice13.cpp
@@ -12,15 +12,24 @@
 #include <gl/glm/gtc/type_ptr.hpp>
 
 
-
 CONST INT WIDTH = 800;
 CONST INT HEIGHT = 600;
 CONST INT LISTSIZE = 4;
+
 std::random_device rd;
 std::default_random_engine dre(rd());
 std::uniform_real_distribution<float> urd{ 0.f,1.f };
 std::uniform_int_distribution uid{ 10, 25 };
 std::uniform_int_distribution uid_pos{ 100,500 };
+
+struct Shape {
+	float shape_face[4][3]{};
+	float colors[4][3]{ {0,0,1},{0,0,1},{0,0,1},{0,0,1} };
+	GLuint vao, vbo[2];
+};
+
+Shape cube[6]{};
+
 //--- 메인 함수
 // 
 // 
@@ -47,17 +56,35 @@ GLuint vao, vbo[2];
 GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수 
 GLuint vertexShader, fragmentShader; //--- 세이더 객체
 GLuint shaderProgramID; //--- 셰이더 프로그램
-float trishape[3][3]{ {0.2,0,0.2},{-0.2,0,0.4},{0,0.5,-0.5} };
-float colors[3][3]{ {0.2,0.4,0.1},{0.2,0.6,0.3},{0.8,0.5,0.1} };
-float line1[2][2]{ {-1,0},{1,0} };
-float line2[2][2]{ {0,-1},{0,1} };
+GLuint shaderProgramID1; //--- 셰이더 프로그램
 
+float cube1[4][3]{ {-0.2,-0.2,0},{0.2,-0.2,0},{-0.2,0.2,0},{0.2,0.2,0} };
+float cube2[4][3]{ {-0.2,-0.2,-0.2},{0.2,-0.2,-0.2},{-0.2,0.2,-0.2 },{0.2,0.2,-0.2} };
+
+
+float cube3[4][3]{ {0.2,-0.2,0},{0.2,-0.2,-0.2},{0.2,0.2,0},{0.2,0.2,-0.2} };
+float cube4[4][3]{ {-0.2,-0.2,0},{-0.2,-0.2,-0.2},{-0.2,0.2,0},{-0.2,0.2,-0.2} };
+
+float cube5[4][3]{ {-0.2,-0.2,0},{-0.2,-0.2,-0.2},{-0.2,0.2,0},{-0.2,0.2,-0.2} };
+float cube6[4][3]{ {-0.2,0.2,0},{-0.2,0.2,-0.2},{0.2,0.2,0},{0.2,0.2,-0.2} };
+
+
+float colors1[4][3]{ {0,0,1},{0,0,1},{0,0,1},{0,0,1} };
+float colors2[4][3]{ {0,1,0},{0,1,0},{0,1,0},{0,1,0} };
+float colors3[4][3]{ {1,0,0},{1,0,0},{1,0,0},{1,0,0} };
+
+float trishape[3][3]{ {0.2,0,0.2},{-0.2,0,0.4},{0,0.5,-0.5} };
+
+float line[6][3]{ {-1,0,0},{1,0,0}, {0,-1,0},{0,1,0}, {0,0,-1},{0,0,1} };
+float line1[2][3]{ {-1,0,0},{1,0,0} };
+float line2[2][3]{ {0,-1,0},{0,1,0} };
+float line3[2][3]{ {0,0,-1},{0,0,1} };
 
 void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 {
 	//--- 윈도우 생성하기
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA| GLUT_DEPTH);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Example1");
@@ -77,7 +104,9 @@ GLvoid drawScene()
 	//--- 변경된 배경색 설정
 	glClearColor(1, 1, 1, 1.0f);
 	//glClearColor(1.0, 1.0, 1.0, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
 	//--- 렌더링 파이프라인에 세이더 불러오기
 	glUseProgram(shaderProgramID);
 	glm::mat4 Rz = glm::mat4(1.0f); //--- 회전 행렬 선언
@@ -85,9 +114,13 @@ GLvoid drawScene()
 	glm::mat4 TR = glm::mat4(1.0f);
 
 
+	
+
+
 	//--- 적용할 모델링 변환 행렬 만들기
 	Tx = glm::translate(Tx, glm::vec3(0.5, 0.0, 0.0));
-	Rz = glm::rotate(Rz, glm::radians(10.0f), glm::vec3(1.0, 1.0, 0.0));
+	Rz = glm::rotate(Rz, glm::radians(10.0f), glm::vec3(1.0, 0.0, 0.0));
+	Rz = glm::rotate(Rz, glm::radians(10.0f), glm::vec3(0.0, 1.0, 0.0));
 	TR = Rz;
 
 	//--- 세이더 프로그램에서 modelTransform 변수 위치 가져오기
@@ -95,8 +128,69 @@ GLvoid drawScene()
 	//--- modelTransform 변수에 변환 값 적용하기
 	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(TR));
 	//--- 도형 그리기
+	
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f); //--- 투영 공간 설정: [-100.0, 100.0]
+	unsigned int projectionLocation = glGetUniformLocation(shaderProgramID, "projectionTransform"); //--- 투영 변환 값 설정
+	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);
+
+
 	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(line1), &line1);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors1), &colors1);
+	glDrawArrays(GL_LINES, 0, 2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(line2), &line2);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors2), &colors2);
+	glDrawArrays(GL_LINES, 0, 2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(line3), &line3);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors3), &colors3);
+	glDrawArrays(GL_LINES, 0, 2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube1), &cube1);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors1), &colors1);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube2), &cube2);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors1), &colors1);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube3), &cube3);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors2), &colors2);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube4), &cube4);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors2), &colors2);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube5), &cube5);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors3), &colors3);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cube6), &cube6);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(colors3), &colors3);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
 	glutSwapBuffers();
 }
 
@@ -121,7 +215,7 @@ void InitBuffer()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	//--- 변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다.
 	//--- triShape 배열의 사이즈: 9 * float
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), trishape, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), trishape, GL_STATIC_DRAW);
 	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	//--- attribute 인덱스 0번을 사용가능하게 함
@@ -130,7 +224,7 @@ void InitBuffer()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	//--- 변수 colors에서 버텍스 색상을 복사한다.
 	//--- colors 배열의 사이즈: 9 *float
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), NULL, GL_STATIC_DRAW);
 	//--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	//--- attribute 인덱스 1번을 사용 가능하게 함.
@@ -153,7 +247,6 @@ void make_shaderProgram()
 	//--- Shader Program 사용하기
 	glUseProgram(shaderProgramID);
 }
-
 
 
 void make_vertexShaders()
